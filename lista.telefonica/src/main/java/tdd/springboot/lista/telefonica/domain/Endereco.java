4f3137fb1846964da9873685ad63f97752e3ad4d
erick cardoso syade <e.syadetbo@gmail.com>
2020-08-09 00:17:57 -0300

    Teste que verifica o retorno do atributo string logradouro falhando.

---
+++ lista.telefonica/src/main/java/tdd/springboot/lista/telefonica/domain/Endereco.java
@@ -0,0 +1,24 @@
+package tdd.springboot.lista.telefonica.domain;
+
+import java.io.Serializable;
+
+public class Endereco implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void setLogradouro(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public short getLogradouro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
---
+++ lista.telefonica/src/test/java/tdd/springboot/lista/telefonica/domain/EnderecoTeste.java
@@ -0,0 +1,24 @@
+package tdd.springboot.lista.telefonica.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EnderecoTeste {
+
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Endereco endereco = new Endereco();
+	}
+
+	@Test
+	void verificaLogradouroVazio() {
+		String resultado = "Conjunto Maguari";
+		Endereco endereco = new Endereco();
+		endereco.setLogradouro(resultado);
+		assertEquals(resultado, endereco.getLogradouro());
+	}
+
+}
