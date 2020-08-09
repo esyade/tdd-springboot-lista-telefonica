1b0ddaefe9f382830f10e794d88b02e78ae339a8
erick cardoso syade <e.syadetbo@gmail.com>
2020-08-08 21:14:06 -0300

    Commit inicial

---
+++ lista.telefonica/src/test/java/tdd/springboot/lista/telefonica/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.springboot.lista.telefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
