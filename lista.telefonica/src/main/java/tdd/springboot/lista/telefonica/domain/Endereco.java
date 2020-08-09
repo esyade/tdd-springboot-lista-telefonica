bcae9c37e8e13780aac76f23a5d3525ca3542078
erick cardoso syade <e.syadetbo@gmail.com>
2020-08-09 00:28:32 -0300

    Teste de verificacao do atributo logradouro, da classe endereco, ok

--- lista.telefonica/src/main/java/tdd/springboot/lista/telefonica/domain/Endereco.java
+++ lista.telefonica/src/main/java/tdd/springboot/lista/telefonica/domain/Endereco.java
@@ -8,14 +8,16 @@ public class Endereco implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String logradouro;
 
 	public void setLogradouro(String string) {
 		// TODO Auto-generated method stub
+		this.logradouro = string ;
 	}
 
-	public short getLogradouro() {
+	public String getLogradouro() {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.logradouro;
 	}
 	
 	
